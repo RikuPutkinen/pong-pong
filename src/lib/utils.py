cd74a3bfd20928dc645fa60db1522744e3530a9a
Riku Putkinen <putkinenriku@gmail.com>
2023-12-20 02:20:49 +0200

    Finish game for now

--- src/lib/utils.py
+++ src/lib/utils.py
@@ -1,5 +1,6 @@
 import pygame
 import math
+import random
 
 class Ball():
     def __init__(self, x, y, angle):
@@ -12,7 +13,7 @@ class Ball():
         self.rect.centerx = x
         self.rect.centery = y
     
-    def update_pos(self, screen, dt):
+    def update_pos(self, dt):
         (speed, angle) = pygame.math.Vector2.as_polar(self.vector)
         self.x += dt * speed * math.cos(math.radians(angle))
         self.y += dt * speed * math.sin(math.radians(angle))
@@ -58,3 +59,125 @@ class Paddle():
         elif (direction == "down") and (self.rect.bottom < height):
             self.pos += 300 * dt
         self.rect.centery = self.pos
+
+class Button():
+    def __init__(self, text, x, y, id):
+        self.text = text
+        self.x = x
+        self.y = y
+        self.id = id
+
+    def draw(self, screen, font, game_state):
+        action = False
+
+        border = pygame.Surface((204, 104))
+        border = border.convert()
+        if (self.id == game_state.selected_button_id):
+            border.fill("white")
+        else:
+            border.fill("black")
+
+        text_background = pygame.Surface((200, 100))
+        text_background = text_background.convert()
+        text_background.fill("black")
+        
+        text = pygame.font.Font.render(font, self.text, 1, "white")
+        text_rect = text.get_rect(center=(text_background.get_width() / 2, text_background.get_height() / 2))
+        
+        text_background.blit(text, text_rect)
+        border.blit(text_background, (2, 2))
+        screen.blit(border, (self.x, self.y))
+
+        keys = pygame.key.get_pressed()
+        if (keys[pygame.K_RETURN]) and (self.id == game_state.selected_button_id):
+            action = True
+        
+        return action
+
+class GameState():
+    def __init__(self):
+        self.dt = 0
+        self.player_score = 0
+        self.computer_score = 0
+        self.balls = []
+        self.bounces = 0
+        self.added = False
+        self.running = True
+        self.selected_button_id = 0
+
+def end_screen(screen, font, game_state):
+    (width, height) = screen.get_size()
+    rematch_button = Button("Rematch", 100, 300, 0)
+    exit_button = Button("Quit", 400, 300, 1)
+
+    if (game_state.player_score < game_state.computer_score):
+            text = pygame.font.Font.render(font, "You lost", 1, "white")
+    elif (game_state.player_score > game_state.computer_score):
+        text = pygame.font.Font.render(font, "You won", 1, "white")
+    else:
+        text = pygame.font.Font.render(font, "Tie", 1, "white")
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_LEFT]:
+        game_state.selected_button_id = 0
+    elif keys[pygame.K_RIGHT]:
+        game_state.selected_button_id = 1
+
+    text_rect = text.get_rect(center=(width/2, height/2))
+    screen.fill("black")
+    screen.blit(text, text_rect)
+    
+    if rematch_button.draw(screen, font, game_state):
+        game_state.__init__()
+    if exit_button.draw(screen, font, game_state):
+        game_state.running = False
+
+
+def continue_game(screen, font, game_state, player_paddle, computer_paddle):
+    (width, height) = screen.get_size()
+    angles = list(range(-45, 46)) + list(range(135, 226))
+
+    if (game_state.bounces % 3 == 0 and game_state.added == False) or (len(game_state.balls) == 0):
+        new_ball = Ball(width / 2, random.randint(30, height - 30), random.choice(angles))
+        game_state.balls.append(new_ball)
+        game_state.added = True
+
+    keys = pygame.key.get_pressed()
+    if keys[pygame.K_UP]:
+        player_paddle.move_paddle(screen, game_state.dt, "up")
+    elif keys[pygame.K_DOWN]:
+        player_paddle.move_paddle(screen, game_state.dt, "down")
+
+    closest_ball = max(game_state.balls, key=lambda item: item.x)
+    if (closest_ball.y < computer_paddle.rect.centery):
+        computer_paddle.move_paddle(screen, game_state.dt, "up")
+    elif (closest_ball.y > computer_paddle.rect.centery):
+        computer_paddle.move_paddle(screen, game_state.dt, "down") 
+
+    screen.fill("black")
+    pygame.draw.rect(screen, "white", player_paddle)
+    pygame.draw.rect(screen, "white", computer_paddle)
+
+    to_pop = []
+    for i in range(len(game_state.balls)):
+        game_state.balls[i].update_pos(game_state.dt)
+        res = game_state.balls[i].check_collision(screen, [player_paddle, computer_paddle])
+        if (res > 0):
+            to_pop.append(i)
+            if (res == 1): game_state.computer_score += 1
+            elif (res == 2): game_state.player_score += 1
+        if (res == 0):
+            game_state.bounces += 1
+            game_state.added = False
+
+        game_state.balls[i].draw(screen)
+
+    to_pop.reverse()
+    for i in to_pop:
+        game_state.balls.pop(i)
+
+    text = pygame.font.Font.render(font, f"{game_state.player_score} | {game_state.computer_score}", 1, "white")
+    text_rect = text.get_rect()
+    text_rect.centerx = width / 2
+    text_rect.top = 10
+    screen.blit(text, text_rect)
---
+++ src/main.py
@@ -0,0 +1,33 @@
+import pygame
+from lib.utils import Paddle, GameState, continue_game, end_screen
+
+size = width, height = (640, 480)
+
+pygame.init()
+screen = pygame.display.set_mode(size)
+pygame.display.set_caption("Pong Pong")
+clock = pygame.time.Clock()
+font = pygame.font.SysFont("ubuntumono", 32)
+score_limit = 1
+
+
+player_paddle = Paddle(screen)
+computer_paddle = Paddle(screen)
+computer_paddle.rect.right = width
+
+game_state = GameState()
+
+while game_state.running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            game_state.running = False
+
+    if (game_state.player_score < score_limit) and (game_state.computer_score < score_limit):
+        continue_game(screen, font, game_state, player_paddle, computer_paddle)
+    else:
+        end_screen(screen, font, game_state)
+    
+    pygame.display.flip()
+    game_state.dt = clock.tick(60) / 1000
+
+pygame.quit()
